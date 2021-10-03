031c42519ff8141d1e87431c6b59f712e3ba1ea8
neelbshah18 <89878257+neelbshah18@users.noreply.github.com>
2021-10-02 20:35:10 -0400

    Liner Regression

---
+++ Linear Regression_final_model.py
@@ -0,0 +1,18 @@
+import pickle
+var = input("Enter News Headline ")
+print("You entered: " + str(var))
+
+
+#function to run for prediction
+def detecting_fake_news(var):    
+#retrieving the best model for prediction call
+    load_model = pickle.load(open('final_model.sav', 'rb'))
+    prediction = load_model.predict([var])
+    prob = load_model.predict_proba([var])
+
+    return (print("The given statement is ",prediction[0]),
+        print("The truth probability score is ",prob[0][1]))
+
+
+if __name__ == '__main__':
+    detecting_fake_news(var)
